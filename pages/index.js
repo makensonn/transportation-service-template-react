d08eab72a7680b40f5c84833cad7644396f5635d
Makenson Noel <makensonnoel@Makensons-Air.attlocal.net>
2023-07-22 15:53:05 -0400

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Main from '../components/Main'
-import About from '../components/about'
+import About from '../components/About'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
 
