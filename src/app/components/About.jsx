012b066e2547cfdbebb85abc717f6ec88625f97b
Restyled.io <commits@restyled.io>
2024-01-12 00:38:52 +0000

    Restyled by prettier

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,45 +1,50 @@
-'use client'
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import GuittoImg from '../assets/about.png';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import GuittoImg from "../assets/about.png";
 
-
-const titleStyle = 'text-4xl font-bold inline border-b-4 border-red-600'
+const titleStyle = "text-4xl font-bold inline border-b-4 border-red-600";
 
 const About = () => {
   return (
-    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
-      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-        <div className='col-span-2'>
-          <p className={titleStyle}><br></br><br></br>About</p>
-          <p className='py-2'></p>
-          <p className='py-2'>
-            Welcome to Guitto Transportation, where luxury car services meet elegance and distinction. 
-            We specialize in providing unparalleled transportation experiences for airport transfers, 
-            events, and funeral services. 
-          <br></br><br></br>
-            Our exclusive fleet of meticulously maintained luxury vehicles 
-            ensures that every journey is a testament to opulence and sophistication. Whether you&#39;re a 
-            discerning traveler seeking a seamless airport transfer, a couple celebrating a milestone event, 
-            or a family requiring compassionate support during a funeral, our professional chauffeurs will 
-            cater to your every need with utmost care and discretion.
-          <br></br><br></br>
-            At Guitto Transportation, we take glory in 
-            delivering exceptional service, ensuring that each moment is cherished, and every detail is 
-            meticulously attended to, leaving you with unforgettable memories of the finest transportation experience.
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className={titleStyle}>
+            <br></br>
+            <br></br>About
+          </p>
+          <p className="py-2"></p>
+          <p className="py-2">
+            Welcome to Guitto Transportation, where luxury car services meet
+            elegance and distinction. We specialize in providing unparalleled
+            transportation experiences for airport transfers, events, and
+            funeral services.
+            <br></br>
+            <br></br>
+            Our exclusive fleet of meticulously maintained luxury vehicles
+            ensures that every journey is a testament to opulence and
+            sophistication. Whether you&#39;re a discerning traveler seeking a
+            seamless airport transfer, a couple celebrating a milestone event,
+            or a family requiring compassionate support during a funeral, our
+            professional chauffeurs will cater to your every need with utmost
+            care and discretion.
+            <br></br>
+            <br></br>
+            At Guitto Transportation, we take glory in delivering exceptional
+            service, ensuring that each moment is cherished, and every detail is
+            meticulously attended to, leaving you with unforgettable memories of
+            the finest transportation experience.
           </p>
           <br></br>
-
         </div>
-        <div className='w-auto h-auto m-auto shadow-xl shadow-neutral-950 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={GuittoImg} className='rounded-xl' alt='/' />
+        <div className="w-auto h-auto m-auto shadow-xl shadow-neutral-950 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <Image src={GuittoImg} className="rounded-xl" alt="/" />
         </div>
       </div>
     </div>
-
-    
   );
 };
 
